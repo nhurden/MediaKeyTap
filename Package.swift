b9c6268a8ff8f8eb67cfc47bbff8cb7427102a65
Daniel Barber <github@danbarber.me>
2019-09-27 11:12:25 -0400

    Add Package.swift file

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "MediaKeyTap",
+    targets: [
+        .target(
+            name: "MediaKeyTap",
+            path: "MediaKeyTap"
+        ),
+    ]
+)
